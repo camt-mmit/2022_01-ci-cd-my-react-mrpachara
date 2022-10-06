004420d29d86146fadc8016a6dc387d7e557574a
Pachara Tinamas <mrpachara@gmail.com>
2022-10-07 05:25:28 +0700

    test 30

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn v2.0 React
         </a>
       </header>
     </div>
