cfd5ac987baede4e962ae93bf609c964f09910c3
Pachara Tinamas <mrpachara@gmail.com>
2022-10-07 10:28:28 +0700

    test fetch change head

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn yyyy React
         </a>
       </header>
     </div>
