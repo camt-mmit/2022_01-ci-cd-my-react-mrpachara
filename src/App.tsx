fd209b51ee81d48b7a06d4bc0f5d4b665245de0f
Pachara Tinamas <mrpachara@gmail.com>
2022-10-10 12:52:59 +0700

    test PUSH_MAIN

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test feature-01
+          Learn React Test PUSH_MAIN
         </a>
       </header>
     </div>
